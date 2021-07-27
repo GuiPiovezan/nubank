620754b21019f911656683eca639a26085f3a48a
Guilherme Ap Ferreira Piovezan <guilherme.piovezan00@gmail.com>
2021-07-27 08:17:07 -0300

    Unused Components Excluded

--- src/components/Snackbar/colors.ts
+++
@@ -1,7 +0,0 @@
-import { Colors } from 'react-native-paper'
-
-export const SnackbarColors = {
-  SUCCESS: Colors.green700,
-  ERROR: Colors.red700,
-  WARNING: Colors.yellow700,
-}
--- src/screens/App/HomeScreen/components/Loader/index.web.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { ActivityIndicator } from 'react-native-paper'
-
-const Loader: React.FC = () => <ActivityIndicator />
-
-export default Loader
